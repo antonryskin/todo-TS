d9389548ca16be1baf5b8e9a42bb383400592d1f
Anton <stepasha587@mail.ru>
2021-04-06 20:44:41 +0300

    add checkbox for tasks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import { Itodo } from '../../Interfaces/interfaces';
 import Navbar from '../Navbar/Navbar';
 import TodoForm from '../TodoForm/TodoForm';
+import { TodoList } from '../TodoList/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<Itodo[]>([])
 
   const addHandler = (title: string) => {
-    console.log('Add new todo', title);
+    const newTodo: Itodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    setTodos(prev => [newTodo, ...prev])
     
   }
   return (
@@ -14,6 +21,7 @@ function App() {
       <Navbar/>
       <div className="container">
         <TodoForm onAdd={addHandler}/>
+        <TodoList todos={todos}/>
       </div>
     </>
   );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { Itodo } from "../../Interfaces/interfaces";
 
 type TodoListProps = {
-  todos: any[];
+  todos: Itodo[];
 };
 
 export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
   return (
     <ul>
       {todos.map((todo) => {
+          const classes = ['todo']
+          if (todo.completed) {
+            classes.push('completed')
+          }
         return (
-          <li className="todo">
+          <li className={classes.join(' ')} key={todo.id}>
             <label>
-              <input type="checkbox" />
-              <span></span>
+              <input type="checkbox" checked={todo.completed}/>
+              <span>{todo.title}</span>
               <i className="material-icons red-text">Delete</i>
             </label>
           </li>
